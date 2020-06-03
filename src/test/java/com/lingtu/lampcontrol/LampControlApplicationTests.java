ecbba654a1f9f9830f2d7d6eb96162f32f8c2483
zhufusheng <zhufusheng@lingtu.com>
2020-06-03 17:15:43 +0800

    Initial commit

---
+++ src/test/java/com/lingtu/lampcontrol/LampControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingtu.lampcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
