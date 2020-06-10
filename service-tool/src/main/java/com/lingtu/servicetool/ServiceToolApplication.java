3a713ad9ddd3b6c1133ec97325541bd36101410d
zhufusheng <zhufusheng@lingtu.com>
2020-06-10 11:25:11 +0800

    mybatis逆向工程

---
+++ service-tool/src/main/java/com/lingtu/servicetool/ServiceToolApplication.java
@@ -0,0 +1,13 @@
+package com.lingtu.servicetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceToolApplication.class, args);
+    }
+
+}
